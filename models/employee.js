9d527298b99b59a92a0ba6d982a6e90b3ac07da3
Maverick Wolfe <maverick.m.wolfe@gmail.com>
2024-02-07 10:25:46 -0700

    setup folder structure, created sequelize models

---
+++ models/employee.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Employee extends Model {}
+
+Employee.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    role_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "employee",
+        key: "id",
+      },
+    },
+    manager_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "employee",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "employee",
+  }
+);
+
+module.exports = Employee;
